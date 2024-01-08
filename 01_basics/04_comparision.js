a4f4670d90a2d626e6cbde98231ce7c2293eb4f3
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-01-08 20:57:18 +0000

    Comparision in javascript

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+// avoid these type of comparisions
+// console.log("1" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// // console.log(null == 0);
+// // console.log(null <= 0);
+
+// // === this is strict checking method
+// console.log("2" === 2);
