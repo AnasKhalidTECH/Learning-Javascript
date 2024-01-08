257bca9c80b2fd4b4dbadd4861e13fea1f186f9f
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-01-08 20:36:27 +0000

    Conversion and Operations completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,24 @@
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** Operations *******************************
+
+let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = "Anas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3+4) * 5 % 3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
