ec703dba673349ca630161d1ea2889fc8619c059
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-01-13 01:47:43 +0500

    math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,34 @@
+const score = 4000
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(score.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 208.456
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//***************************** Maths **********************************//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.3));
+// console.log(Math.max(2,1,5,9));
+// console.log(Math.min(10,1,2,5));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
