c79919f1668c5e120c20cd9a7a667742fe74b34f
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-01-15 00:11:48 +0500

    basics of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const mentors = ["Qasim Ali Shah", "Hisham Sarwar", "Junaid Akram"]
+
+const newArr = new Array(1, 2, 3, 4, 5, 6)
+// console.log(myArr);
+
+// Array Methods
+
+// myArr.push(6) it adds the value in the end of an array
+// myArr.pop() it removes the value from the end of an array
+// myArr.unshift(8) it adds the value in the start of an array
+// myArr.shift() it removes the value from the start of an array
+// console.log(myArr.includes(8)); include used in checking wether the value is present in an array or not - it returns true or false
+
+// const newArr2 = myArr.join() it converts array into a string
+
+// console.log(myArr);
+// console.log(newArr2);
+
+// Slice, Splice
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1, 3) Slice returns the copy of a section of an array
+
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(1, 3) Splice removes the selected range of an array
+// console.log("C", myArr);
+// console.log(myn2);
