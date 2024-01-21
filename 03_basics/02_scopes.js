e2d1d991c30cf95b79e83694071006209ddc548b
AnasKhalidTECH <anaskhalid938@gmail.com>
2024-01-21 22:58:28 +0500

    global and local scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+function one(){
+    const username = "Anas"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+if (true){
+    const username = "Anas"
+    if(username === "Anas"){
+        const website = " Google"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//*******************Interesting ***********************/
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo  = function(num){
+    return num + 2
+}
